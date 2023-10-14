675b89c92b622b0be68f0988c9915f6ce9495487
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-15 05:53:36 +0600

    - api added to fetch all todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,35 @@
-import ListContainer from "./ListContainer";
 import Todo from "./Todo";
+import ListContainer from "./ListContainer";
+import { useGetTodosQuery } from "@redux/features/todo/todoApi";
+import { selectFilterReducer } from "@redux/features/filter/filterSlice";
+import { useAppSelector } from "@redux/hooks";
 
 export default function TodoList() {
+  const { _order, _sort, color, text_like, completed } =
+    useAppSelector(selectFilterReducer);
+
+  const {
+    data: todos,
+    isLoading,
+    isFetching,
+    isError,
+  } = useGetTodosQuery({ _order, _sort, color, completed, text_like });
+
+  if (isLoading && !isError) {
+    return <p className="text-center">Loading...</p>;
+  }
+
+  if (!isLoading && isError) {
+    return <p>Something went wrong</p>;
+  }
+
+  if (!isLoading && !isFetching && !isError && !todos?.length) {
+    return <p className="text-center">No todo found</p>;
+  }
+
   return (
     <ListContainer>
-      {[]?.map((todo) => <Todo todo={todo} key={todo.id} />)}
+      {todos?.map((todo) => <Todo todo={todo} key={todo.id} />)}
     </ListContainer>
   );
 }
