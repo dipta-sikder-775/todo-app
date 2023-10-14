702582ffc5bbe647c9dc74ccf3c81c5e392b73e4
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-14 23:20:08 +0600

    - vite config added for production optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+  build: {
+    cssMinify: true,
+    minify: true,
+  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://example.com/api/v1",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       ws: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
