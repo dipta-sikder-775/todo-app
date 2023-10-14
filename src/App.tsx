2c2655b4e0968468adbc587054ec1c7430e9afb1
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-15 04:58:59 +0600

    - updated component combination

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import Card from '@components/Card'
-import React from 'react'
+import Card from "@components/Card";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import Navbar from "@components/Navbar";
+import TodoList from "@components/TodoList";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <main
       className="min-h-screen"
+      // className="relative grid min-h-screen place-items-center gap-0 space-y-2 px-6"
     >
       <Navbar />
 
       <Card>
-      <hr className="mt-4" />
+        <Header />
 
-<ListContainer>
-  <Todo/>
-</ListContainer>
+        <hr className="mt-4" />
+
+        <TodoList />
+
+        <hr className="mt-4" />
+
+        <Footer />
       </Card>
-    </main>
-  )
-}
 
-export default App
+      <Toaster position="top-right" />
+    </main>
+  );
+};
+
+export default App;
