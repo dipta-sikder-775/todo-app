8c516ea42e1e902e7ca7133f5bd994e165723312
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-15 02:21:03 +0600

    - Navbar designed

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="w-full bg-indigo-600 py-4 text-center text-lg font-bold text-white shadow-lg">
+      Todo App
+    </div>
+  );
+}
